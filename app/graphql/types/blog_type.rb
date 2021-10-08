e4469a49155362dc8a51ef9e17c4121fe1cc8697
Amadou <amadou140589@gmail.com>
2021-10-08 18:00:10 +0100

    graphql implemented

--- app/graphql/types/blog_type.rb
+++ app/graphql/types/blog_type.rb
@@ -2,8 +2,9 @@ module Types
   class BlogType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: true
-    field :text, String, null: true,
-    field :created_at, GraphQl::Types::IS08601DateTime, null: false
-    field :updated_at, GraphQl::Types::IS08601DateTime, null: false
+    field :text, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+   
   end
 end
